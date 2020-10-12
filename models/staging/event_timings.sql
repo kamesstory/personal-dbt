35038749b17b0d8cbfb8fae7552a56b168c448ba
Boris Jabes <boris@getcensus.com>
2020-10-12 21:19:13 +0000

    add better event sessionization code

--- models/staging/event_timings.sql
+++ models/staging/event_timings.sql
@@ -1,21 +1,24 @@
--- PART 1: data prep on each user's events to find time since their last event, if the next event constitutes a new session and is on a new day than the previous
-with data as (
-  select
-    user_id,
-    "time",
-    round(((extract(epoch from time) - extract(epoch from lag(time) over (partition by user_id order by time asc))) / 60)::decimal, 1) minutes_since_last_event,
-    case
-      when coalesce(round(((extract(epoch from time) - extract(epoch from lag(time) over (partition by user_id order by time asc))) / 60)::decimal, 1), 30) >= 30 then 1
-      else 0
-    end as new_session,
-    case
-      when date_trunc('day', time) <> date_trunc('day', lag(time) over (partition by user_id order by time asc)) then 1
-      when lag(time) over (partition by user_id order by time asc) is null then 1
-      else 0
-    end as new_day
-  from {{ source('marker', 'events') }}
-  order by 2, 1 asc
+WITH sessions AS (
+    --for each session, create a unique session id by concatenating the user_id with its sequential session number
+    --define the start timestamp and (effective) end timestamp for each session 
+    SELECT
+        ROW_NUMBER() OVER (PARTITION BY lag.user_id ORDER BY lag.time) || '-' || lag.user_id AS session_id
+        , lag.user_id
+        , lag.idle_minutes as idle_minutes_prior
+        , lag.time AS session_start
+        , LEAD(lag.time) OVER (PARTITION BY lag.user_id ORDER BY lag.time) AS next_session_start
+        , DATEDIFF(minutes, session_start, next_session_start) AS session_length_in_minutes
+    --count the number of idle minutes between each track event by user_id
+    FROM (
+        SELECT 
+            user_id
+            , "time"
+            , DATEDIFF(minutes, LAG(time) OVER(PARTITION BY user_id ORDER BY time), time) AS idle_minutes
+        FROM {{ source('marker', 'events') }}
+    ) AS lag
+    --define a new session when at least 120 minutes have elapsed since the last event (or when this is the first event) for each user_id
+    WHERE (lag.idle_minutes > 120 OR lag.idle_minutes IS NULL)
 )
 
-select *
-from data
+
+select * from sessions
