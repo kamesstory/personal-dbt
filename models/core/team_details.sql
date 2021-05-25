dedc525fed905cdbf694ab692e7d0e5d09942cca
Sean Lynch <sean@getcensus.com>
2021-05-25 10:22:12 -0700

    Second attempt to cast booleans

--- models/core/team_details.sql
+++ models/core/team_details.sql
@@ -1,7 +1,7 @@
 select
     *,
     -- extract preferences JSON
-    json_extract_path_text(preferences, 'self_upgrade') :: boolean as self_upgrade,
-    json_extract_path_text(preferences, 'public_sharing') :: boolean as public_sharing,
-    json_extract_path_text(preferences, 'members_can_invite') :: boolean as members_can_invite
+    json_extract_path_text(preferences, 'self_upgrade') = 'true' as self_upgrade,
+    json_extract_path_text(preferences, 'public_sharing') = 'true' as public_sharing,
+    json_extract_path_text(preferences, 'members_can_invite') = 'true' as members_can_invite
 from {{ source('marker', 'teams') }}
