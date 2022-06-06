5b3441b2f3ef75cad2df857badb9b1b79caf71af
Jason Wang <jason@getcensus.com>
2022-06-06 10:59:42 -0700

    basic test macro

---
+++ macros/test.sql
@@ -0,0 +1,12 @@
+-- We can get the config alias (if there is one) using config.get
+-- If there's no alias, we need the model name
+
+{% macro config() %}
+  {{ log("Running macro config()...", true) }}
+  {{ log("Kwargs:", true) }}
+  {{ log(kwargs, true) }}
+
+  {% set _dummy = kwargs.update({'materialized': 'view', 'schema': 'test'}) %}
+  {{ log(kwargs, true) }}
+  {{ builtins.config(kwargs) }}
+{% endmacro %}
