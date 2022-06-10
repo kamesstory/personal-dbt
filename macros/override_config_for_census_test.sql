a097d102c1ccc83a50a9afa28a11e21b715931d1
Jason Wang <jason@getcensus.com>
2022-06-10 09:28:13 -0700

    committing existing code

--- macros/override_config_for_census_test.sql
+++ macros/override_config_for_census_test.sql
@@ -5,11 +5,16 @@
 
   {{ log("Overriding config for Census test with macro override_config_for_census_test...", true) }}
 
+  {{ log("Model currently is " ~ model, true) }}
+
   {% set base_model_name = model['name'] %}
-  {% set current_name = builtins.config.get('alias') or base_model_name %}
+  {% set base_alias = config.get('alias') %}
+  {{ log("Base alias is " ~ base_alias, true) }}
+
+  {% set current_name = config.get('alias') or base_model_name %}
   {% set overriding_name = current_name ~ '_123456' %}
 
   {% set new_config = {'materialized': 'view', 'schema': 'census', 'alias': overriding_name} %}
-  {{ return(builtins.config(new_config)) }}
+  {{ return(config(new_config)) }}
 
 {% endmacro %}
