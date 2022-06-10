5ab3420b372aa418ff2dc18c59559e4913fa5f03
Jason Wang <jason@getcensus.com>
2022-06-10 09:20:41 -0700

    even better

--- macros/override_config_for_census_test.sql
+++ macros/override_config_for_census_test.sql
@@ -1,14 +1,23 @@
 -- We can get the config alias (if there is one) using config.get
--- If there's no alias, we need the model name
-
--- Need to find the config constructor so that this macro properly handles both
--- the inline .sql config() case and the usage inside another macro config.get()
+-- If there's no alias, we need the model name (coalesce using python's or)
 
 {% macro override_config_for_census_test() %}
 
   {{ log("Overriding config for Census test with macro override_config_for_census_test...", true) }}
+  {{ log("Model:" ~ model, true) }}
 
-  {% set new_config = {'materialized': 'view', 'schema': 'census'} %}
+  {% set model_name = model['name'] %}
+  {{ log("Model name:" ~ model_name, true) }}
+
+  {% set existing_name = builtins.config.get('alias') or model_name %}
+
+  {{ log("Existing name = " ~ existing_name, true) }}
+
+  {% set alias = existing_name ~ '_123456' %}
+
+  {{ log("New alias = " ~ alias, true)}}
+
+  {% set new_config = {'materialized': 'view', 'schema': 'census', 'alias': alias} %}
   {{ return(builtins.config(new_config)) }}
 
 {% endmacro %}
